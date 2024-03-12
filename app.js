c964c746ca73b557eb182fb9d22540b727c5e4a9
arir2001 R <amtrooney@gmail.com>
2024-03-12 19:51:31 +0000

    if statements added for rock paper scissors game in js file.

--- app.js
+++ app.js
@@ -8,18 +8,66 @@ const result = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 
 let userChoice
+let computerChoice
+let resulting
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => 
-{
-   userChoice = e.target.id
-   UserChoiceDisplay.innerHTML = userChoice
+{   userChoice = e.target.id
+    UserChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 
-   generateComputerChoice()
-
+    generateResult()
 }))
 
-function generate generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length +1 )
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) +1
 
-    console.log(randomNumber)
+    if (randomNumber ===1){
+        computerChoice = 'rock'
+    }
+
+    if (randomNumber ===2){
+        computerChoice = 'scissors'
+    }
+
+    if (randomNumber ===3){
+        computerChoice = 'paper'
+    }
+    ComputerChoiceDisplay.innerHTML = computerChoice
+}
+
+function generateResult() {
+    if (computerChoice == userChoice){
+        resulting = 'draw!'
+    }
+
+    if(computerChoice == 'paper' && userChoice == 'rock'){
+        resulting = 'Computer wins!'
+    }
+
+
+    if(computerChoice == 'scissors' && userChoice == 'paper') {
+        resulting = 'Computer wins!'
+    }
+
+
+    if(computerChoice == 'rock' && userChoice == 'scissors'){
+        resulting = 'Computer wins!'
+    }
+
+    if(userChoice == 'paper' && computerChoice == 'rock'){
+        resulting = 'You win!'
+    }
+
+
+    if(userChoice == 'scissors' && computerChoice == 'paper') {
+        resulting = 'You win!'
+    }
+
+
+    if(userChoice == 'rock' && computerChoice == 'scissors'){
+        resulting = 'You win!'
+    }
+
+    result.innerHTML = resulting
 }
