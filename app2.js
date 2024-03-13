0d0cfc5eebbadc5ffb7887f83325d1a36ddb3b12
arir2001 R <amtrooney@gmail.com>
2024-03-13 19:53:15 +0000

    basaic functions for creating the card flip and the grip in the mempory game added.

--- app2.js
+++ app2.js
@@ -61,16 +61,37 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random()) 
 
 
-
+const cardChosen = []
 const gridDisplay = document.querySelector("#grid")
 
 function createBoard() {
-    for (let i = 0; i< 10; i++){
+    for (let i = 0; i < cardArray.length; i++){
         const card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('src', 'assets/pics/oldguy.jpg')
         card.setAttribute('data-id', i)
+
+        card.addEventListener('click', flipCard)
+
         console.log(card, i)
+
+        gridDisplay.appendChild(card)
     }
 }
 
 createBoard()
+
+function flipCard() {
+    //finding out the way the aarray has been randomly sorted
+    console.log(cardArray)
+    // "this" keyword allows whatever item we clicked to be noted
+    // we get the data id of the chosen card
+    const cardId = this.getAttribute('data-id')
+    //then we find the caard's place in the shuffled aarray
+    console.log(cardArray[cardId].name)
+    //placing the chosen cards into the cardchosen rray
+    cardChosen.push(cardArray[cardId].name)
+}
+
+
+
+
