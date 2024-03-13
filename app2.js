d11887d3f5831ed308207b52ef2aaf842786ab06
arir2001 R <amtrooney@gmail.com>
2024-03-13 23:08:58 +0000

    memory game functions etc complete.

--- app2.js
+++ app2.js
@@ -61,8 +61,12 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random()) 
 
 
-const cardChosen = []
+let cardChosen = []
+let cardChosenIds = []
+
 const gridDisplay = document.querySelector("#grid")
+const resultDisplay = document.querySelector("#result")
+
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++){
@@ -72,25 +76,77 @@ function createBoard() {
 
         card.addEventListener('click', flipCard)
 
-        console.log(card, i)
-
         gridDisplay.appendChild(card)
     }
 }
 
 createBoard()
 
-function flipCard() {
-    //finding out the way the aarray has been randomly sorted
-    console.log(cardArray)
-    // "this" keyword allows whatever item we clicked to be noted
-    // we get the data id of the chosen card
-    const cardId = this.getAttribute('data-id')
-    //then we find the caard's place in the shuffled aarray
-    console.log(cardArray[cardId].name)
-    //placing the chosen cards into the cardchosen rray
-    cardChosen.push(cardArray[cardId].name)
+//gaathering all the image tags into a const
+//pushing the image ids into 
+
+i = 0
+function checkMatch(){
+    const cards = document.querySelectorAll('#grid img')
+    
+    if (cardChosenIds[1] == cardChosenIds[0]){
+        alert("You chose the same image twice")
+
+        cards[cardChosenIds[0]].setAttribute('src', 'assets/pics/oldguy.jpg')
+        console.log[cardChosenIds[1]]
+
+
+    } else if (cardChosen[1] == cardChosen[0] ){
+
+        if (cardChosenIds[1] != cardChosenIds[0]){
+
+        console.log(cardChosenIds[0], cardChosenIds[1])
+        alert('You found a match')
+
+        cards[cardChosenIds[0]].setAttribute('src', 'assets/pics/smiley.png')
+        cards[cardChosenIds[1]].setAttribute('src', 'assets/pics/smiley.png')
+
+        i ++ 
+
+        console.log(i)
+        console.log(cards.length/2)
+
+        if (i == cards.length/2){
+            console.log()
+            resultDisplay.textContent = 'Congrats you won!'
+        }
+
+    }} else {
+        cards[cardChosenIds[0]].setAttribute('src', 'assets/pics/oldguy.jpg')
+        cards[cardChosenIds[1]].setAttribute('src', 'assets/pics/oldguy.jpg')  
+    }
+
+
+
+    cardChosen = []
+    cardChosenIds = []
+
 }
+
+//finding out the way the aarray has been randomly sorted
+// "this" keyword allows whatever item we clicked to be noted
+// we get the data id of the chosen card
+//then we find the caard's place in the shuffled aarray
+//placing the chosen cards into the cardchosen array
+// set the new source of the image to now be the image of the id in the aarray.
+
+function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardChosen.push(cardArray[cardId].name)
+    cardChosenIds.push(cardId)
+    
+    this.setAttribute('src', cardArray[cardId].img)
+
+    if (cardChosen.length == 2){
+        setTimeout( checkMatch, 900)
+    }
+}
+
 
 
 
