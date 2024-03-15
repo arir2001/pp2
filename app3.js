04f9115c4399ad7c572a7a865146164e493c2b32
arir2001 R <amtrooney@gmail.com>
2024-03-15 21:12:59 +0000

    whackamole game mostly done, now just needs styling and level 2 + 3

--- app3.js
+++ app3.js
@@ -1,4 +1,7 @@
 const squares = document.querySelectorAll('.square');
+
+const soil = document.querySelector('.soil');
+
 const mole = document.querySelector('.mole');
 const trick = document.querySelector('.trick');
 const friend = document.querySelector('.friend');
@@ -6,19 +9,44 @@ const friend = document.querySelector('.friend');
 const mybutton =  document.querySelector("#mybigbutton")
 const mybutton2 =  document.querySelector("#mybigbutton2")
 
-let timeleft = document.querySelector('#time-left');
+const level2Button =  document.querySelector("#level2")
+const level3Button =  document.querySelector("#level3")
 
 let time = 60;
 const endtime = 60;
-const score = document.querySelector('#score');
+
+let score = document.querySelector('#score');
+let oopsiescore = document.querySelector('#oopsiescore');
+let molescore = document.querySelector('#molescore');
+
 let result  = 0;
+let oopsie  = 0;
+let molestrick  = 0;
 
 
-// choosing a random square from the array, making it blue behind. 
+//score alerts
+
+function scoreCounter(){
+    if (score.textContent == 4){
+        alert("you win!");
+        
+        startstop(true);
+    }
+    if (oopsiescore.textContent == 4){
+        alert("you have killed many rabbits. You lose!");
+        startstop(true);
+    }
+    if (molescore.textContent == 4){
+        alert("The moles just stole your hammer. You lose!");
+        startstop(true);
+    }
+}
+
+
+//assigning the classes to each square
 function randomMole(){
-    
+    //to 3 choose random numbers
     var arr = [];
-
     while(arr.length < 3){
         var r = Math.floor(Math.random() * 8) + 1;
         if(arr.indexOf(r) === -1) arr.push(r);
@@ -27,29 +55,70 @@ function randomMole(){
     let b = arr[1];
     let c = arr[2];
 
+    //to clear the previous square classes
     squares.forEach(square => {
         klass = square.getAttribute("class");
 
-        console.log('this is', klass);
+        square.classList.remove('soil');
 
         square.classList.remove('mole');
         square.classList.remove('friend');
         square.classList.remove('trick');
     } )
 
+    //to assign new squares to the classes
     let randomMole= squares[a];
-    randomMole.classList.add('mole');
+    randomMole.classList.add('soil');
 
     let randomFriend= squares[b];
-    randomFriend.classList.add('friend');
+    randomFriend.classList.add('soil');
 
     let randomTrick= squares[c];
-    randomTrick.classList.add('trick');
+    randomTrick.classList.add('soil');
 
-    console.log('this is mole' , randomMole);
-    console.log('this is friend' , randomFriend);
-    console.log('this is trick' , randomTrick);
+    function PopUp(){
+        randomMole.classList.remove('soil');
+        randomFriend.classList.remove('soil');
+        randomTrick.classList.remove('soil');
+
+        randomMole.classList.add('mole');
+        randomFriend.classList.add('friend');
+        randomTrick.classList.add('trick');
+    }
+
+    timerPopUp = setTimeout(PopUp, 1000)
+
+    scoreCounter()
+
+    // to know which id has been hit
+    hitPosition = randomMole.id;
+    hitFriend = randomFriend.id;
+    hitTrick = randomTrick.id;
 }
+
+
+//if a square with the id hit-x has been hit, the point is added to the appropriate score. 
+squares.forEach(square => {
+    square.addEventListener('mousedown' , () => {
+        if (square.id == hitPosition){
+            result ++
+            score.textContent = result;
+            hitPosition = null;
+        }
+
+        if (square.id == hitFriend){
+            oopsie ++
+            oopsiescore.textContent = oopsie;
+            hitFriend = null;
+        }
+
+        if (square.id == hitTrick){
+            molestrick ++
+            molescore.textContent = molestrick;
+            hitTrick = null;
+        }
+    })
+} )
 
 
 //now start-stop function which allows the game to be started and restarted using buttons. 
@@ -62,28 +131,66 @@ function startstop(stop){
     if (stop == false){
         console.log('start button pressed');
         moveMole2()
+        mybutton.disabled = true;
+        mybutton2.disabled = false;
     
     }else if (stop == true){
         console.log('stopped!');
-        clearInterval(timermole);
-        clearInterval(timertrick);
-        clearInterval(timerfriend);
+
+        clearInterval(timergame);
+        clearTimeout(timerPopUp, 1300);
+        clearInterval(countdownTimer);
+        currentTime = 60;
+        timeleft.textContent = currentTime;
+
+        molescore.textContent = 0;
+        score.textContent = 0;
+        oopsiescore.textContent = 0;
+
+        mybutton.disabled = false; //disabling buttons once clicked
+        mybutton2.disabled = true;
 
         //clearing the game
         squares.forEach(square => {
+            square.classList.remove('soil');
             square.classList.remove('mole');
             square.classList.remove('friend');
-            square.classList.remove('trick');})
-
+            square.classList.remove('trick');
+        })
     }
 }
 
+
+//timer: 
+let timeleft = document.querySelector('#time-left');
+let currentTime = 60;
+
+
+//starting the game!
 function moveMole2(){
-    timermole = setInterval(randomMole, 1000);
-    timertrick = setInterval(randomMole, 1000);
-    timerfriend = setInterval(randomMole, 1000);
+    //assigning squares new classes every 1400 ms
+    timergame = setInterval(randomMole, 1400);
+
+    //ending the game once the 60 secs are over
+    function countDown(){
+        currentTime--
+        timeleft.textContent = currentTime;
+    
+        if (currentTime ==0){
+            alert("GAME OVER!")
+            startstop(true)
+        }
+    }
+    
+    countdownTimer = setInterval(countDown, 1000)
 };
 
-mybutton.addEventListener('click', startstop())
-mybutton2.addEventListener('click', startstop())
+
+
+//buttons to start and stop the game
+mybutton.addEventListener('click' , startstop() )
+mybutton2.addEventListener('click' , startstop() )
+
+
+
 
