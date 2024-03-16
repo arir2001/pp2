93b83df3d3a32c317ee344dd998f3d311897a529
arir2001 R <amtrooney@gmail.com>
2024-03-16 14:46:12 +0000

    updated whackaamole with difficulty levels.

--- app3.js
+++ app3.js
@@ -1,5 +1,6 @@
 const squares = document.querySelectorAll('.square');
 
+
 const mound = document.querySelector('.mound');
 
 const molehint = document.querySelector('.molehint');
@@ -13,59 +14,29 @@ const trick = document.querySelector('.trick');
 const mybutton =  document.querySelector("#mybigbutton")
 const mybutton2 =  document.querySelector("#mybigbutton2")
 
+const level1Button =  document.querySelector("#level1")
 const level2Button =  document.querySelector("#level2")
 const level3Button =  document.querySelector("#level3")
 
+const easyButton =  document.querySelector("#easy")
+const harderButton =  document.querySelector("#harder")
+const hardestButton =  document.querySelector("#hardest")
+
+//variable 
 let time = 60;
+
 const endtime = 60;
 
+// names for the elements selectors
 let score = document.querySelector('#score');
 let oopsiescore = document.querySelector('#oopsiescore');
 let molescore = document.querySelector('#molescore');
 
+//names for the actual score.
 let result  = 0;
 let oopsie  = 0;
 let molestrick  = 0;
 
-
-
-//to 3 choose random numbers
-var arr = [];
-var lis = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-var newlis = []
-var newlis2 = []
-var newlis3 = []
-
-
-//to 3 choose random numbers
-while(arr.length < 3){
-    var r = Math.floor(Math.random() * 8) + 1;
-    if(arr.indexOf(r) === -1) arr.push(r);
-}
-let a = arr[0];
-let b = arr[1];
-let c = arr[2];
-
-for (let i = 0; i < 9; i++) {
-    if (lis[i] !== a) {
-        console.log('i, one', lis[i])
-        newlis.push(lis[i]);
-    }
-}
-
-for (let i = 0; i < 8; i++) {
-    if (newlis[i] !== b) {
-        console.log('i, two', newlis[i])
-        newlis2.push(newlis[i]);
-    }
-}
-
-for (let i = 0; i < 7; i++) {
-    if (newlis2[i] !== c) {
-        console.log('i, three', newlis2[i])
-        newlis3.push(newlis2[i]);
-    }
-}
 
 //score alerts
 function scoreCounter(){
@@ -83,6 +54,44 @@ function scoreCounter(){
     }
 }
 
+
+assigning = 0 //starting values
+popping = 0 //pops up the 
+
+x = 0
+function easiness(x){
+    if (x == 0){
+        alert('choose a difficulty level before you begin!')
+        return x=0
+    }
+
+    if (x == 1){
+        console.log('easy button pressed');
+        easyButton.disabled = false;
+        harderButton.disabled = true;
+        hardestButton.disabled = true;
+
+        return assigning = 1800 , popping = 1200
+    }
+
+    if (x == 2){
+        console.log('harder button pressed');
+        easyButton.disabled = true;
+        harderButton.disabled = false;
+        hardestButton.disabled = true;
+
+        return assigning = 1800 , popping = 1600
+    }
+
+    if (x == 3){
+        console.log('hardest button pressed');
+        easyButton.disabled = true;
+        harderButton.disabled = true;
+        hardestButton.disabled = false;
+ 
+        return assigning = 1300 , popping = 1100 
+    }
+}
 
 //assigning the classes to each square
 function randomMole(){
@@ -105,19 +114,16 @@ function randomMole(){
 
     for (let i = 0; i < 9; i++) {
         if (lis[i] !== a) {
-            console.log('i, one', lis[i])
             newlis.push(lis[i]);
         }
     }
     for (let i = 0; i < 8; i++) {
         if (newlis[i] !== b) {
-            console.log('i, two', newlis[i])
             newlis2.push(newlis[i]);
         }
     }
     for (let i = 0; i < 7; i++) {
         if (newlis2[i] !== c) {
-            console.log('i, three', newlis2[i])
             newlis3.push(newlis2[i]);
         }
     }//this is the list of squares without mole, friend or trick. 
@@ -134,36 +140,29 @@ function randomMole(){
         square.classList.remove('friend');
         square.classList.remove('trick');
 
-        //to make the behind look like mounds. 
-        for(let i = 0; i < 7; i++) {
-            let attach = squares[newlis2[i]];
-            attach.classList.add('mound')
-            }
     } )
 
+    //to make the behind look like mounds. 
+    for(let i = 0; i < 7; i++) {
+        squares[newlis2[i]].classList.add('mound')
+        }
     //to assign new hint squares
     let randomMole= squares[a];
-    randomMole.classList.add('molehint');
-
     let randomFriend= squares[b];
-    randomFriend.classList.add('friendhint');
-
     let randomTrick= squares[c];
-    randomTrick.classList.add('trickhint');
 
+    randomMole.classList.add('molehint');
+    randomFriend.classList.add('friendhint');
+    randomTrick.classList.add('trickhint');
 
     //assign the actual pop ups moles, rabbits, trick
     function PopUp(){
-        randomMole.classList.add('molehint');
-        randomFriend.classList.add('friendhint');
-        randomTrick.classList.add('trickhint');
-
         randomMole.classList.add('mole');
         randomFriend.classList.add('friend');
         randomTrick.classList.add('trick');
     }
 
-    timerPopUp = setTimeout(PopUp, 1000)
+    timerPopUp = setTimeout(PopUp, popping)
 
     scoreCounter()
 
@@ -207,25 +206,19 @@ let stop = null;
 function startstop(stop){
     if (stop == false){
         console.log('start button pressed');
-        moveMole2()
-        mybutton.disabled = true;
-        mybutton2.disabled = false;
+        console.log(assigning, popping)
+
+        if (assigning != 0){
+            moveMole2()
+            mybutton.disabled = true;
+            mybutton2.disabled = false;
+        }else{
+            alert("choose a difficulty!")
+        }
+        
     
     }else if (stop == true){
         console.log('stopped!');
-
-        clearInterval(timergame);
-        clearTimeout(timerPopUp);
-        clearInterval(countdownTimer);
-        currentTime = 60;
-        timeleft.textContent = currentTime;
-
-        molescore.textContent = 0;
-        score.textContent = 0;
-        oopsiescore.textContent = 0;
-
-        mybutton.disabled = false; //disabling buttons once clicked
-        mybutton2.disabled = true;
 
         //clearing the game
         squares.forEach(square => {
@@ -234,11 +227,36 @@ function startstop(stop){
             square.classList.remove('molehint');
             square.classList.remove('friendhint');
             square.classList.remove('trickhint');
-    
+
             square.classList.remove('mole');
             square.classList.remove('friend');
             square.classList.remove('trick');
+
+            console.log('entered')
         })
+
+        clearInterval(timergame);
+        clearTimeout(timerPopUp);
+        clearInterval(countdownTimer);
+        currentTime = 60;
+        timeleft.textContent = currentTime;
+
+        result  = 0;
+        oopsie  = 0;
+        molestrick  = 0;
+
+        molescore.textContent = 0;
+        score.textContent = 0;
+        oopsiescore.textContent = 0;
+        assigning = 0;
+        popping = 0;
+
+        mybutton.disabled = false; //disabling buttons once clicked
+        mybutton2.disabled = true;
+
+        easyButton.disabled = false;
+        harderButton.disabled = false;
+        hardestButton.disabled = false;
     }
 }
 
@@ -251,7 +269,7 @@ let currentTime = 60;
 //starting the game!
 function moveMole2(){
     //assigning squares new classes every 1400 ms
-    timergame = setInterval(randomMole, 100);
+    timergame = setInterval(randomMole, assigning);
 
     //ending the game once the 60 secs are over
     function countDown(){
