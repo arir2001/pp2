bc4edfb65894e54c9e5f6f11496a837155d4ae11
arir2001 R <amtrooney@gmail.com>
2024-03-16 02:02:41 +0000

    whackamole game updated.

--- app3.js
+++ app3.js
@@ -1,10 +1,14 @@
 const squares = document.querySelectorAll('.square');
 
-const soil = document.querySelector('.soil');
+const mound = document.querySelector('.mound');
+
+const molehint = document.querySelector('.molehint');
+const friendhint = document.querySelector('.friendhint');
+const trickhint = document.querySelector('.trickhint');
 
 const mole = document.querySelector('.mole');
-const trick = document.querySelector('.trick');
 const friend = document.querySelector('.friend');
+const trick = document.querySelector('.trick');
 
 const mybutton =  document.querySelector("#mybigbutton")
 const mybutton2 =  document.querySelector("#mybigbutton2")
@@ -24,12 +28,49 @@ let oopsie  = 0;
 let molestrick  = 0;
 
 
-//score alerts
 
+//to 3 choose random numbers
+var arr = [];
+var lis = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var newlis = []
+var newlis2 = []
+var newlis3 = []
+
+
+//to 3 choose random numbers
+while(arr.length < 3){
+    var r = Math.floor(Math.random() * 8) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+let a = arr[0];
+let b = arr[1];
+let c = arr[2];
+
+for (let i = 0; i < 9; i++) {
+    if (lis[i] !== a) {
+        console.log('i, one', lis[i])
+        newlis.push(lis[i]);
+    }
+}
+
+for (let i = 0; i < 8; i++) {
+    if (newlis[i] !== b) {
+        console.log('i, two', newlis[i])
+        newlis2.push(newlis[i]);
+    }
+}
+
+for (let i = 0; i < 7; i++) {
+    if (newlis2[i] !== c) {
+        console.log('i, three', newlis2[i])
+        newlis3.push(newlis2[i]);
+    }
+}
+
+//score alerts
 function scoreCounter(){
     if (score.textContent == 4){
         alert("you win!");
-        
         startstop(true);
     }
     if (oopsiescore.textContent == 4){
@@ -47,6 +88,13 @@ function scoreCounter(){
 function randomMole(){
     //to 3 choose random numbers
     var arr = [];
+    var lis = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    var newlis = []
+    var newlis2 = []
+    var newlis3 = []
+
+
+    //to 3 choose random numbers
     while(arr.length < 3){
         var r = Math.floor(Math.random() * 8) + 1;
         if(arr.indexOf(r) === -1) arr.push(r);
@@ -55,31 +103,60 @@ function randomMole(){
     let b = arr[1];
     let c = arr[2];
 
+    for (let i = 0; i < 9; i++) {
+        if (lis[i] !== a) {
+            console.log('i, one', lis[i])
+            newlis.push(lis[i]);
+        }
+    }
+    for (let i = 0; i < 8; i++) {
+        if (newlis[i] !== b) {
+            console.log('i, two', newlis[i])
+            newlis2.push(newlis[i]);
+        }
+    }
+    for (let i = 0; i < 7; i++) {
+        if (newlis2[i] !== c) {
+            console.log('i, three', newlis2[i])
+            newlis3.push(newlis2[i]);
+        }
+    }//this is the list of squares without mole, friend or trick. 
+
     //to clear the previous square classes
     squares.forEach(square => {
-        klass = square.getAttribute("class");
+        square.classList.remove('mound');
 
-        square.classList.remove('soil');
+        square.classList.remove('molehint');
+        square.classList.remove('friendhint');
+        square.classList.remove('trickhint');
 
         square.classList.remove('mole');
         square.classList.remove('friend');
         square.classList.remove('trick');
+
+        //to make the behind look like mounds. 
+        for(let i = 0; i < 7; i++) {
+            let attach = squares[newlis2[i]];
+            attach.classList.add('mound')
+            }
     } )
 
-    //to assign new squares to the classes
+    //to assign new hint squares
     let randomMole= squares[a];
-    randomMole.classList.add('soil');
+    randomMole.classList.add('molehint');
 
     let randomFriend= squares[b];
-    randomFriend.classList.add('soil');
+    randomFriend.classList.add('friendhint');
 
     let randomTrick= squares[c];
-    randomTrick.classList.add('soil');
+    randomTrick.classList.add('trickhint');
 
+
+    //assign the actual pop ups moles, rabbits, trick
     function PopUp(){
-        randomMole.classList.remove('soil');
-        randomFriend.classList.remove('soil');
-        randomTrick.classList.remove('soil');
+        randomMole.classList.add('molehint');
+        randomFriend.classList.add('friendhint');
+        randomTrick.classList.add('trickhint');
 
         randomMole.classList.add('mole');
         randomFriend.classList.add('friend');
@@ -138,7 +215,7 @@ function startstop(stop){
         console.log('stopped!');
 
         clearInterval(timergame);
-        clearTimeout(timerPopUp, 1300);
+        clearTimeout(timerPopUp);
         clearInterval(countdownTimer);
         currentTime = 60;
         timeleft.textContent = currentTime;
@@ -152,7 +229,12 @@ function startstop(stop){
 
         //clearing the game
         squares.forEach(square => {
-            square.classList.remove('soil');
+            square.classList.remove('mound');
+
+            square.classList.remove('molehint');
+            square.classList.remove('friendhint');
+            square.classList.remove('trickhint');
+    
             square.classList.remove('mole');
             square.classList.remove('friend');
             square.classList.remove('trick');
@@ -169,7 +251,7 @@ let currentTime = 60;
 //starting the game!
 function moveMole2(){
     //assigning squares new classes every 1400 ms
-    timergame = setInterval(randomMole, 1400);
+    timergame = setInterval(randomMole, 100);
 
     //ending the game once the 60 secs are over
     function countDown(){
